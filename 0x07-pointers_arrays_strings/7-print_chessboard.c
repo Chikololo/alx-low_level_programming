29370c1b7765d66508daeec960e5edba70ea4e5c
Chikololo <chikololommbetsa@gmail.com>
2023-08-01 04:47:36 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,10 @@
-#insert "main.h"
+#include "main.h"
 /**
  * print_chessboard - Entry point
  * @a: array
  * Return: Always 0 (Success)
  */
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
 {
 	int i;
 	int j;
